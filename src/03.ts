6e4c4d06db926f79633fe626f79e808bc7d7221b
Florian Richter <flori@hey.com>
2024-12-03 10:38:45 +0100

    Add day 3

--- src/03.ts
+++ src/03.ts
@@ -7,8 +7,28 @@ const client = new AocClient({
   token: process.env.AOC_SESSION!,
 });
 
-function part1(input: string) {}
+function part1(input: string) {
+  let sum = 0;
+  for (const result of input.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/gm)) {
+    sum += parseInt(result[1]) * parseInt(result[2]);
+  }
+  return sum;
+}
 
-function part2(input: string) {}
+function part2(input: string) {
+  return input
+    .replaceAll("\n", "")
+    .split(/don't\(\).*?do\(\)/gm)
+    .reduce(
+      (sum, enabledSection, i, l) =>
+        sum +
+        part1(
+          i === l.length - 1
+            ? enabledSection.split("don't()")[0]
+            : enabledSection
+        ),
+      0
+    );
+}
 
 await client.run([part1 as PartFn, part2 as PartFn], false);
