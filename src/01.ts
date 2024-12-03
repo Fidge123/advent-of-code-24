226d72b151928247e77e363b17c5028e71f09d91
Florian Richter <flori@hey.com>
2024-12-03 10:02:26 +0100

    Implement day 1 and 2

--- src/01.ts
+++ src/01.ts
@@ -7,8 +7,34 @@ const client = new AocClient({
   token: process.env.AOC_SESSION!,
 });
 
-function part1(input: string) {}
+function part1(input: string) {
+  const leftList = input
+    .split("\n")
+    .map((line) => parseInt(line.split("   ")[0]))
+    .sort((a, b) => a - b);
+  const rightList = input
+    .split("\n")
+    .map((line) => parseInt(line.split("   ")[1]))
+    .sort((a, b) => a - b);
+  return leftList.reduce(
+    (sum, location, index) => sum + Math.abs(location - rightList[index]),
+    0
+  );
+}
 
-function part2(input: string) {}
+function part2(input: string) {
+  const leftList = input
+    .split("\n")
+    .map((line) => parseInt(line.split("   ")[0]));
+  const rightList = input
+    .split("\n")
+    .map((line) => parseInt(line.split("   ")[1]));
+  return leftList.reduce(
+    (similarity, location) =>
+      similarity +
+      location * rightList.filter((loc) => loc === location).length,
+    0
+  );
+}
 
 await client.run([part1 as PartFn, part2 as PartFn], false);
--- src/02.ts
+++ src/02.ts
@@ -7,8 +7,76 @@ const client = new AocClient({
   token: process.env.AOC_SESSION!,
 });
 
-function part1(input: string) {}
+function part1(input: string) {
+  return input
+    .split("\n")
+    .map((report) => report.split(" ").map((level) => parseInt(level, 10)))
+    .filter(
+      (report) =>
+        !!report.reduce((prev, curr) => {
+          const diff = curr - prev;
+          return diff >= 1 && diff <= 3 && prev !== 0 ? curr : 0;
+        }) ||
+        !!report.reduce((prev, curr) => {
+          const diff = prev - curr;
+          return diff >= 1 && diff <= 3 && prev !== 0 ? curr : 0;
+        })
+    ).length;
+}
 
-function part2(input: string) {}
+function part2(input: string) {
+  return input
+    .split("\n")
+    .map((report) => report.split(" ").map((level) => parseInt(level, 10)))
+    .filter((report) => {
+      // console.log();
+      // console.log(report);
+      return checkSafety(report, 1, 3) || checkSafety(report, -3, -1);
+    }).length;
+}
+
+function checkSafety(report: number[], min: number, max: number) {
+  let safe = true;
+  let dampener = true;
+
+  for (let i = 1; i < report.length; i++) {
+    const diff = report[i] - report[i - 1];
+    const prevDiff = report[i] - report[i - 2];
+    const nextDiff = report[i + 1] - report[i - 1];
+    const skipDiff = report[i + 1] - report[i];
+
+    if (outside(diff, min, max)) {
+      if (!dampener) {
+        // console.log("second ❌", i, diff);
+        safe = false;
+        break;
+      }
+
+      if (between(nextDiff, min, max)) {
+        i++;
+        dampener = false;
+      } else if (between(prevDiff, min, max)) {
+        dampener = false;
+      } else if (i === 1 && between(skipDiff, min, max)) {
+        i++;
+        dampener = false;
+      } else if (i !== report.length - 1) {
+        // console.log("impossible ❌", i, diff);
+        safe = false;
+        break;
+      }
+    }
+  }
+
+  return safe;
+}
+
+function outside(input: number, min: number, max: number) {
+  return input < min || input > max;
+}
+
+function between(input: number, min: number, max: number) {
+  return input >= min && input <= max;
+}
 
 await client.run([part1 as PartFn, part2 as PartFn], false);
