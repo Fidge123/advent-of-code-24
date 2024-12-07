4e52ffab7e66aa5d651baba908811a698fd1a514
Florian Richter <flori@hey.com>
2024-12-07 19:46:39 +0100

    Solve day 7

--- src/07.ts
+++ src/07.ts
@@ -7,8 +7,62 @@ const client = new AocClient({
   token: process.env.AOC_SESSION!,
 });
 
-function part1(input: string) {}
+function part1(input: string) {
+  return input
+    .split("\n")
+    .map((line) => line.split(": "))
+    .map(([result, input]) => [
+      parseInt(result),
+      ...input.split(" ").map((i) => parseInt(i)),
+    ])
+    .reduce(
+      (sum, [result, ...inputs]) =>
+        testOperators(result, inputs.reverse()) ? sum + result : sum,
+      0
+    );
+}
 
-function part2(input: string) {}
+function testOperators(result: number, inputs: number[]): boolean {
+  if (inputs.length === 1) {
+    return result === inputs[0];
+  }
+
+  return (
+    testOperators(result / inputs[0], inputs.slice(1)) ||
+    testOperators(result - inputs[0], inputs.slice(1))
+  );
+}
+
+function part2(input: string) {
+  return input
+    .split("\n")
+    .map((line) => line.split(": "))
+    .map(([result, input]) => [
+      parseInt(result),
+      ...input.split(" ").map((i) => parseInt(i)),
+    ])
+    .reduce((sum, [result, first, ...inputs]) => {
+      const foo = testOperators2(result, first, inputs);
+      console.log(result, inputs, foo);
+      return foo ? sum + result : sum;
+    }, 0);
+}
+
+function testOperators2(
+  result: number,
+  first: number,
+  inputs: number[]
+): boolean {
+  if (inputs.length === 0) {
+    return result === first;
+  }
+  const concat = parseInt(first.toString() + inputs[0].toString());
+
+  return (
+    testOperators2(result, concat, inputs.slice(1)) ||
+    testOperators2(result, first + inputs[0], inputs.slice(1)) ||
+    testOperators2(result, first * inputs[0], inputs.slice(1))
+  );
+}
 
 await client.run([part1 as PartFn, part2 as PartFn], false);
